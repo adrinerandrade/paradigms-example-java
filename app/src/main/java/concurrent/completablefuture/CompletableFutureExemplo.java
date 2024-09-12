7f69c2878887df8946850efd9209ad56a1e447ea
Adriner Andrade <adriner_andrade@hotmail.com>
2024-09-12 14:47:57 -0300

    fix bug

--- app/src/main/java/concurrent/completablefuture/CompletableFutureExemplo.java
+++ app/src/main/java/concurrent/completablefuture/CompletableFutureExemplo.java
@@ -1,31 +1,34 @@
 package concurrent.completablefuture;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class CompletableFutureExemplo {
 
     public static void main(String[] args) {
         // Criar uma tarefa assíncrona com CompletableFuture
-        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return 42;
-        }, Executors.newFixedThreadPool(1));
+        try (ExecutorService executor = Executors.newFixedThreadPool(1)) {
+            CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return 42;
+            }, executor);
 
-        // Processar o resultado sem bloquear a thread principal
-        completableFuture.thenApply(resultado -> {
-            System.out.println("Resultado: " + resultado);
-            return resultado * 2;
-        }).thenAccept(resultadoFinal -> {
-            System.out.println("Resultado Final: " + resultadoFinal);
-        });
+            // Processar o resultado sem bloquear a thread principal
+            completableFuture.thenApply(resultado -> {
+                System.out.println("Resultado: " + resultado);
+                return resultado * 2;
+            }).thenAccept(resultadoFinal -> {
+                System.out.println("Resultado Final: " + resultadoFinal);
+            });
 
-        // Continuar com outras tarefas enquanto a operação assíncrona é executada
-        System.out.println("Tarefa assíncrona em execução...");
+            // Continuar com outras tarefas enquanto a operação assíncrona é executada
+            System.out.println("Tarefa assíncrona em execução...");
+        }
     }
 
 }
