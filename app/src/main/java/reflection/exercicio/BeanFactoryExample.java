ec24173fb5816672d7ad86f72ecd207ab5641274
Adriner Andrade <adriner_andrade@hotmail.com>
2024-10-21 17:25:33 -0300

    feat: melhorando metodo main

--- app/src/main/java/reflection/exercicio/BeanFactoryExample.java
+++ app/src/main/java/reflection/exercicio/BeanFactoryExample.java
@@ -3,11 +3,9 @@ package reflection.exercicio;
 public class BeanFactoryExample {
 
     public static void main(String[] args) {
-        ObjectWithInjectionImpl objectWithInjection1 = new ObjectWithInjectionImpl();
         ObjectWithInjection objectWithInjection = BeanFactory.create(ObjectWithInjection.class);
 
         objectWithInjection.logMyBean();
-        objectWithInjection1.logMyBean();
     }
 
 }
